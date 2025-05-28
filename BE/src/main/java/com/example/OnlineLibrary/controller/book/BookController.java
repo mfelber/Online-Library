8515e139ffd82040fba81b201409bc31706e577e
Marcel Felber <68185700+mfelber@users.noreply.github.com>
2025-05-28 08:56:08 +0200

    save-book endpoint fix

--- BE/src/main/java/com/example/OnlineLibrary/controller/book/BookController.java
+++ BE/src/main/java/com/example/OnlineLibrary/controller/book/BookController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,7 +18,6 @@ import com.example.OnlineLibrary.request.BookRequest;
 import com.example.OnlineLibrary.service.BookService;
 
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
